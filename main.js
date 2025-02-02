5222e256a8a1073d33e31b5d352f89c8486d17c1
Yongyeon Kim <seyeon.dev@gmail.com>
2025-02-03 00:02:07 +0900

    feat: add image modal when click image

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const scrollBar = document.querySelector(".bar div");
 const lastPoint = document.querySelectorAll(".point")[1];
+const slideImgs = document.querySelectorAll(".slide img");
+const modal = document.querySelector("#modal");
 
 document.addEventListener(
   "scroll",
@@ -11,6 +13,25 @@ document.addEventListener(
     else lastPoint.style.backgroundColor = "#d3d3d3";
   }, 300)
 );
+
+slideImgs.forEach((img) =>
+  img.addEventListener("click", (e) => {
+    const modalImg = document.querySelector("#modal img");
+
+    modalImg.src = e.target.src;
+    modalImg.alt = e.target.alt;
+    modalImg.onclick = (e) => {
+      e.stopPropagation();
+    };
+    document.body.style.overflowY = "hidden"; // Prevent scroll
+    modal.style.display = "flex";
+  })
+);
+
+modal.addEventListener("click", () => {
+  modal.style.display = "none";
+  document.body.style.overflowY = "unset";
+});
 
 function throttle(callback, delay) {
   let timerId = null;
